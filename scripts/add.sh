72ce413a5b8d39478ebf479585b3c6cbb76d5891
Berardinux <berardinux@gmail.com>
2023-07-28 10:23:20 -0400

    Jul 28, 10:23

--- scripts/add.sh
+++ scripts/add.sh
@@ -2,6 +2,8 @@
 
 if [ ! -d "/etc/openvpn/.ovpn" ]; then
     sudo mkdir /etc/openvpn/.ovpn
+else
+    sudo rm -r /etc/openvpn/.ovpn/*
 fi
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <source_file>"
@@ -15,19 +17,37 @@ else
     echo "Error: File does not have .ovpn extension."
     exit 1
 fi
+####################################################################################################
+
 nu_switch=$(cat /opt/OpenVPN/OpenVPN | sed -n '2p' | cut -d '"' -f 2)
 new_nu=$((nu_switch+1))
 ln_switch=$(($(cat /opt/OpenVPN/OpenVPN | grep -n '"END" ///' | cut -d ':' -f 1) - 1))
+ovpn_name=$(ls /etc/openvpn/.ovpn/)
+name=$(echo "$ovpn_name" | cut -d "." -f 1)
 
-if [ "$nu_switch" -le 8 ]; then
+if [ -f "/etc/openvpn/client/$ovpn_name" ]; then
+    echo "$ovpn_name Is already used!"
+    exit 1
+else
+    sudo mv /etc/openvpn/.ovpn/$ovpn_name /etc/openvpn/client
+fi
+
+if [ "$((nu_switch))" -le 8 ]; then
     read -r -d '' code_switch << EOM
         # Switch $nu_switch
-        switch_with_label_button$nu_switch = self.create_switch_with_label_button("Label for Switch 1")
-        switch_box.pack_start(switch_with_label_button$nu_switch, False, False, 0)
+            {
+                "label": "$name vpn",
+                "command_on": self.run_command_switch_on,
+                "command_off": self.run_command_switch_off,
+                "command_switch_on": ["pkexec", "openvpn", "/etc/openvpn/client/$ovpn_name"],
+                "command_switch_off": ["pkexec", "killall", "openvpn"],
+            },
     
 EOM
     sudo sed -i "s/##### --\"$nu_switch\"-- #####/##### --\"$new_nu\"-- #####/" "/opt/OpenVPN/OpenVPN"
     awk -v line="$ln_switch" -v code="$code_switch" 'NR==line{print code}1' /opt/OpenVPN/OpenVPN | sudo tee /opt/OpenVPN/OpenVPN >/dev/null
 else
-    echo "$nu_switchs Switchs is the limit"
+    echo "$nu_switch There are to many switchs"
+    exit 1
 fi
+
