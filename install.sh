38df2eb89e8a3b91d956a6763b334afe83f532ce
Berardinux <berardinux@gmail.com>
2023-07-29 16:22:18 -0400

    Jul 29, 16:22

---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+clear
+distro=$(grep -w "NAME" /etc/os-release | cut -d= -f2 | tr -d '"' | cut -d' ' -f1)
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Please run the script with sudo or as root"
+	exit 1
+else
+	current_user=$SUDO_USER
+fi	
+
+if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
+    if dpkg -l | grep -q "openvpn"; then
+        echo "openvpn is aready installed on your system already"
+        sudo chmod 777 scripts/Uninstall.sh
+	    sudo ./scripts/Uninstall.sh
+	    exit 1
+    else
+        echo "OpenVPN is Not installed on your system yet"
+    fi
+elif [ "$distro" = "Arch" ]; then
+    if pacman -Q openvpn >/dev/null 2>&1; then
+        echo "OpenVPN is installed on your system already"
+	    sudo chmod 777 scripts/Uninstall.sh
+        sudo ./scripts/Uninstall.sh
+	    exit 1
+    else
+        echo "OpenVPN is Not installed on your system yet"
+    fi
+else
+    echo "Your Distro can not be installed using this install script sorry!"
+    echo "Distros that can be installed include (Debian / Ubuntu / Arch)"
+    exit 1
+fi
+
+clear
+
+echo "# Are you ready to install OpenVPN? (Y/n) #"
+read -r install
+clear
+
+echo "# Would you like to install the OpenVPN Connect? (Y/n)"
+read -r app
+clear
+
+if [ -z $install ]; then
+    install=Y
+    if [ "$install" = "Y" ]; then
+        if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
+            sudo apt update && sudo apt upgrade -y && sudo apt install openvpn -y
+        elif [ "$distro" = "Arch" ]; then
+            sudo pacman -Syu && sudo pacman -S --noconfirm openvpn
+        else
+            exit
+        fi
+    fi
+else
+    echo "# Okay, run the script again when you are ready to install! #"
+    exit
+fi
+
+sudo mkdir -p /opt/OpenVPN
+sudo cp -r * /opt/OpenVPN
+sudo touch /opt/OpenVPN/ovpn_app.conf
+touch "/home/$current_user/.local/share/applications/OpenVPN.desktop"
+sudo chmod -R 777 /opt/OpenVPN/
+chmod 777 "/home/$current_user/.local/share/applications/OpenVPN.desktop"
+
+cat << EOM | sudo tee -a "/home/$current_user/.local/share/applications/OpenVPN.desktop" >/dev/null
+[Desktop Entry]
+Type=Application
+Name=OpenVPN
+Comment=OpenVPN
+Icon=/opt/OpenVPN/Images/openvpn.png
+Exec=/opt/OpenVPN/OpenVPN
+Terminal=false
+EOM
+
+echo "###### Installation complete! ######"
---
+++ scripts/Uninstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+distro=$(grep -w "NAME" /etc/os-release | cut -d= -f2 | tr -d '"' | cut -d' ' -f1)
+desktop=$(find / -name "OpenVPN.desktop" 2>/dev/null)
+
+echo "Would you like to uninstall OpenVPN? (Y/n)"
+read -r uninstall
+
+if [ -z "$uninstall" ] || [ "$uninstall" = "Y" ]; then
+	if [ "$distro" = "Debian" ] || [ "$distro" = "Ubuntu" ]; then
+    	sudo apt purge openvpn
+	elif [ "$distro" = "Arch" ]; then
+		sudo pacman -Rns openvpn
+	else
+		echo "This Uninstall script will not work on you Distro."
+		exit 1
+	fi
+elif [ "$uninstall" = "n" ]; then
+	echo "Okay then."
+	exit 1
+else
+	echo "That was not a option try again later."
+	exit 1
+fi
+
+sudo rm $desktop
+sudo rm -r /etc/openvpn
+sudo rm -r /opt/OpenVPN
